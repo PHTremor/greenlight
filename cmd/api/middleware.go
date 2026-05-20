7a65d5526617e6c9517c9fdd4e863d9fdd07ae98
Phrank <mwalefrankmwale@gmail.com>
2026-05-20 13:30:53 +0200

    switch to an Individual RateLimiter using client IP addresses

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -24,20 +27,83 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// initialize a new rate limiter that allows 2 request per second
-	// with a maximum of 4 request in a single burst
-	limiter := rate.NewLimiter(2, 4)
+	// a client struct to hold the rate limiter and lastSeen time for each client
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
 
-	// a closure function which closes pver the limiter variable
+	// create a mutex and a map to hold the clients IP addreses and RateLimiters
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// a background goroutine which removed old entries from the clients map once every minute
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// lock the mutex to prevent any rate limiter from making checks
+			// while the cleanup is in process
+			mu.Lock()
+
+			// loop through clients and delete any entries that havent been seen in the last 3 minutes
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			// unlock the mutex after the cleanup
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// limiter.Allow() checks if the request is allowed, if its not
-		// return a 429 Too Many Requests response
-		if !limiter.Allow() {
+		// use realip.fromRequest() to get the client's IP address.
+		ip := realip.FromRequest(r)
+
+		// lock the mutex to prevent this code from executing concurrently
+		mu.Lock()
+
+		// check if the IP address already exists in the map. If it doesn't
+		// initialize a new rateLimiter, add the IP and the rateLimiter to the map
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		// update the lastSeen time of the client
+		clients[ip].lastSeen = time.Now()
+
+		// call the Allow() method on the limiter for the current IP Address
+		// If request is not allowed, unlock the mutex & send a 429 Too Many Request response
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
 
+		// unlock the mutex before calling the next handler in the chain
+		mu.Unlock()
+
 		next.ServeHTTP(w, r)
 	})
+
+	// NOTE: Global Rate Limiter impl.....
+	// // initialize a new rate limiter that allows 2 request per second
+	// // with a maximum of 4 request in a single burst
+	// limiter := rate.NewLimiter(2, 4)
+
+	// // a closure function which closes pver the limiter variable
+	// return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	// limiter.Allow() checks if the request is allowed, if its not
+	// 	// return a 429 Too Many Requests response
+	// 	if !limiter.Allow() {
+	// 		app.rateLimitExceededResponse(w, r)
+	// 		return
+	// 	}
+
+	// 	next.ServeHTTP(w, r)
+	// })
 
 }
